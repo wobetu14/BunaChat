82e18056c61ee1f87bf2fdd605f819c56fd3daa5
Wobetu Shiferaw <wobetushiferaw@gmail.com>
2025-09-25 14:55:42 +0300

    Frontend signup functionality completed.

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,17 @@ import express from 'express';
 import dotenv from 'dotenv'; 
 import path from 'path';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
+import { ENV } from './lib/env.js';
 
 dotenv.config();
 
 const app = express();
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true}))
 const __dirname = path.resolve();
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
+import React, { useEffect } from 'react'
+import { Navigate, Route, Routes } from 'react-router'
 import ChatPage from './pages/ChatPage';
 import LoginPage from './pages/LoginPage';
-import SIgnUpPage from './pages/SIgnUpPage';
+import SignUpPage from './pages/SignUpPage';
 import { useAuthStore } from './store/useAuthStore';
+import PageLoader from './components/PageLoader';
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
-  const { authUser, isLoggedIn, login } = useAuthStore();
-  
-  console.log("User Info:", authUser);
-  console.log("Is Logged In:", isLoggedIn);
+  const { checkAuth, isCheckingAuth, authUser } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth])
+
+  console.log(authUser);
+  console.log(isCheckingAuth);
+
+  if (isCheckingAuth) return <PageLoader /> 
   return (
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
       
@@ -18,15 +26,16 @@ const App = () => {
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
 
-      <div className="z-10 text-slate-300">
-        <button onClick={login} className='btn btn-primary'>Click to login</button>
-      </div>
-
       <Routes>
-        <Route path="/" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SIgnUpPage />} />
+        <Route path="/" element={authUser ? <ChatPage /> : <Navigate to={"/login"} />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"} /> } />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
       </Routes>
+
+      <Toaster
+        position="bottom-center"
+        reverseOrder={false}
+      /> 
     </div>
   );
 }
