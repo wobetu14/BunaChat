8397f9bc0b33c3fec62d71f7bd03210b360974bd
Wobetu Shiferaw <wobetushiferaw@gmail.com>
2025-09-27 21:31:32 +0300

    Chat container setup completed.

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { XIcon } from "lucide-react";
+import { useChatStore } from "../store/useChatStore";
+import { useEffect } from "react";
+// import { useAuthStore } from "../store/useAuthStore";
 
-const ChatHeader = () => {
+function ChatHeader() {
+  const { selectedUser, setSelectedUser } = useChatStore();
+  // const { onlineUsers } = useAuthStore();
+  // const isOnline = onlineUsers.includes(selectedUser._id);
+
+  useEffect(() => {
+    const handleEscKey = (event) => {
+      if (event.key === "Escape") setSelectedUser(null);
+    };
+
+    window.addEventListener("keydown", handleEscKey);
+
+    // cleanup function
+    return () => window.removeEventListener("keydown", handleEscKey);
+  }, [setSelectedUser]);
+
   return (
-    <div>ChatHeader</div>
-  )
-}
+    <div
+      className="flex justify-between items-center bg-slate-800/50 border-b
+   border-slate-700/50 max-h-[84px] px-6 flex-1"
+    >
+      <div className="flex items-center space-x-3">
+        <div className={`avatar online`}>
+          <div className="w-12 rounded-full">
+            <img
+              src={selectedUser.profilePic || "/avatar.png"}
+              alt={selectedUser.fullName}
+            />
+          </div>
+        </div>
 
-export default ChatHeader
+        <div>
+          <h3 className="text-slate-200 font-medium">
+            {selectedUser.fullName}
+          </h3>
+          <p className="text-slate-400 text-sm">
+            {"Online"}
+          </p>
+        </div>
+      </div>
+
+      <button onClick={() => setSelectedUser(null)}>
+        <XIcon className="w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer" />
+      </button>
+    </div>
+  );
+}
+export default ChatHeader;
